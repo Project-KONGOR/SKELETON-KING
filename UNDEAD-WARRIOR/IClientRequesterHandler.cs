dc8c8309e7ebfd5b233c62b0683b32526ab4873d
Mark Klara <mrhappyasthma@gmail.com>
2023-04-23 20:59:17 -0700

    Port over the comments on `IClientRequesterHandler.cs`.

--- UNDEAD-WARRIOR/IClientRequesterHandler.cs
+++ UNDEAD-WARRIOR/IClientRequesterHandler.cs
@@ -2,7 +2,14 @@
 
 using Microsoft.AspNetCore.Mvc;
 
+/// <summary>
+///    Interface for a `client_requester.php` function request handlers.
+/// </summary>
 public interface IClientRequesterHandler
 {
+    /// <summary>
+    ///    Handles a `client_requester.php` request with the given `formData`
+    ///    and returns the appropriate result.
+    /// </summary>
     public Task<IActionResult> HandleRequest(Dictionary<string, string> formData);
 }
