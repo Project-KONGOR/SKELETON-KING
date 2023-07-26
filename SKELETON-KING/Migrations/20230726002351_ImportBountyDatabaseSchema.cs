899b5d2bb40f7bec6116ff6add39e60c59ba487c
tarre <tarre.islam@gmail.com>
2023-07-26 21:46:55 +0200

    Basic migration to create DB schema and root root user

---
+++ SKELETON-KING/Migrations/20230726002351_ImportBountyDatabaseSchema.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using PUZZLEBOX;
+
+#nullable disable
+
+namespace SKELETON_KING.Migrations
+{
+    /// <inheritdoc />
+    [DbContext(typeof(BountyContext))]
+    [Migration("20230726002351_ImportBountyDatabaseSchema")]
+    public partial class ImportBountyDatabaseSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // This will grant a 
+            // File property for sql file  = Always copy
+            var baseDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Migrations");
+            var path = Path.Combine(baseDirectory, "20230726002351_ImportBountyDatabaseSchema.sql");
+            migrationBuilder.Sql(File.ReadAllText(path));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+      
+        }
+    }
+}
