bdd058e0b9f9a91ae3e84c3fa5197de6a8596700
Denis Koroskin <2korden@gmail.com>
2023-11-13 22:12:48 -0600

    Fix errors in ShowSimpleStatsHandler

--- ZORGATH/ShowSimpleStatsHandler.cs
+++ ZORGATH/ShowSimpleStatsHandler.cs
@@ -1,13 +1,60 @@
 ﻿namespace ZORGATH;
 
-public record ShowSimpleStatsData(
+class CombinedPlayerAwardSummary
+{
+    public int MVP;
+    public List<string> Top4Names;
+    public List<int> Top4Nums;
+
+    public static CombinedPlayerAwardSummary AddUp(PlayerAwardSummary pas1, PlayerAwardSummary pas2, PlayerAwardSummary pas3, PlayerAwardSummary pas4)
+    {
+        PlayerAwardSummary playerAwardSummary = new PlayerAwardSummary()
+        {
+            TopAnnihilations = pas1.TopAnnihilations + pas2.TopAnnihilations + pas3.TopAnnihilations + pas4.TopAnnihilations,
+            MostQuadKills = pas1.MostQuadKills + pas2.MostQuadKills + pas3.MostQuadKills + pas4.MostQuadKills,
+            BestKillStreak = pas1.BestKillStreak + pas2.BestKillStreak + pas3.BestKillStreak + pas4.BestKillStreak,
+            MostSmackdowns = pas1.MostSmackdowns + pas2.MostSmackdowns + pas3.MostSmackdowns + pas4.MostSmackdowns,
+            MostKills = pas1.MostKills + pas2.MostKills + pas3.MostKills + pas4.MostKills,
+            MostAssists = pas1.MostAssists + pas2.MostAssists + pas3.MostAssists + pas4.MostAssists,
+            LeastDeaths = pas1.LeastDeaths + pas2.LeastDeaths + pas3.LeastDeaths + pas4.LeastDeaths,
+            TopSiegeDamage = pas1.TopSiegeDamage + pas2.TopSiegeDamage + pas3.TopSiegeDamage + pas4.TopSiegeDamage,
+            MostWardsKilled = pas1.MostWardsKilled + pas2.MostWardsKilled + pas3.MostWardsKilled + pas4.MostWardsKilled,
+            TopHeroDamage = pas1.TopHeroDamage + pas2.TopHeroDamage + pas3.TopHeroDamage + pas4.TopHeroDamage,
+            TopCreepScore = pas1.TopCreepScore + pas2.TopCreepScore + pas3.TopCreepScore + pas4.TopCreepScore,
+            MVP = pas1.MVP + pas2.MVP + pas3.MVP + pas4.MVP,
+        };
+
+        List<KeyValuePair<int, string>> awardToCount = new();
+        awardToCount.Add(new KeyValuePair<int, string>(playerAwardSummary.TopAnnihilations, "awd_mann"));
+        awardToCount.Add(new KeyValuePair<int, string>(playerAwardSummary.MostQuadKills, "awd_mqk"));
+        awardToCount.Add(new KeyValuePair<int, string>(playerAwardSummary.BestKillStreak, "awd_lgks"));
+        awardToCount.Add(new KeyValuePair<int, string>(playerAwardSummary.MostSmackdowns, "awd_msd"));
+        awardToCount.Add(new KeyValuePair<int, string>(playerAwardSummary.MostKills, "awd_mkill"));
+        awardToCount.Add(new KeyValuePair<int, string>(playerAwardSummary.MostAssists, "awd_masst"));
+        awardToCount.Add(new KeyValuePair<int, string>(playerAwardSummary.LeastDeaths, "awd_ledth"));
+        awardToCount.Add(new KeyValuePair<int, string>(playerAwardSummary.TopSiegeDamage, "awd_mbdmg"));
+        awardToCount.Add(new KeyValuePair<int, string>(playerAwardSummary.MostWardsKilled, "awd_mwk"));
+        awardToCount.Add(new KeyValuePair<int, string>(playerAwardSummary.TopHeroDamage, "awd_mhdd"));
+        awardToCount.Add(new KeyValuePair<int, string>(playerAwardSummary.TopCreepScore, "awd_hcs"));
+
+        awardToCount.Sort((a, b) => -a.Key.CompareTo(b.Key));
+        return new CombinedPlayerAwardSummary()
+        {
+            MVP = playerAwardSummary.MVP,
+            Top4Names = awardToCount.Select(a => a.Value).Take(4).ToList(),
+            Top4Nums = awardToCount.Select(a => a.Key).Take(4).ToList(),
+        };
+    }
+}
+
+record ShowSimpleStatsData(
     int TotalLevel,
     int TotalExperience,
     int NumberOfHeroesOwned,
     int TotalMatchesPlayed,
-    int NumberOfMVPAwards,
-    ICollection<string> UnlockedUpgradeCodes,
+    CombinedPlayerAwardSummary CombinedPlayerAwardSummary,
     ICollection<string> SelectedUpgradeCodes,
+    ICollection<string> UnlockedUpgradeCodes,
     int AccountId,
     int SeasonId,
     SeasonShortSummary SeasonNormal,
@@ -15,7 +62,7 @@ public record ShowSimpleStatsData(
 
 public class ShowSimpleStatsHandler : IClientRequesterHandler
 {
-    private const int NumberOfHeroesTheGameHas = 1239;
+    private const int NumberOfHeroesTheGameHas = 139;
 
     // In Heroes of Newerth, there is a slightly different logic when computing stats for Seasons <= 6 and Seasons > 6.
     // Additionally, season id must be < 1000. 22 was chosen somewhat arbitrarily and primarily because revival started
@@ -41,15 +88,24 @@ public class ShowSimpleStatsHandler : IClientRequesterHandler
                 /* TotalLevel: */ 0,
                 /* TotalExperience: */ 0,
                 /* NumberOfHeroesOwned: */ NumberOfHeroesTheGameHas,
-                /* TotalMatchesPlayed: */ 0,
-                /* NumberOfMVPAwards: */ 0,
+                /* TotalMatchesPlayed: */ 
+                    // Public
+                    account.PlayerSeasonStatsPublic.Wins + account.PlayerSeasonStatsPublic.Losses +
+                    // Ranked
+                    account.PlayerSeasonStatsRanked.Wins + account.PlayerSeasonStatsRanked.Losses +
+                    // Casual
+                    account.PlayerSeasonStatsRankedCasual.Wins + account.PlayerSeasonStatsRankedCasual.Losses +
+                    // Mid Wars
+                    account.PlayerSeasonStatsMidWars.Wins + account.PlayerSeasonStatsMidWars.Losses,
+                /* PlayerAwardSummary: */ CombinedPlayerAwardSummary.AddUp(account.PlayerSeasonStatsPublic.PlayerAwardSummary, account.PlayerSeasonStatsRanked.PlayerAwardSummary, account.PlayerSeasonStatsRankedCasual.PlayerAwardSummary, account.PlayerSeasonStatsMidWars.PlayerAwardSummary),
                 /* SelectedUpgrades: */ account.SelectedUpgradeCodes,
                 /* UnlockedUpgradeCodes: */ account.User.UnlockedUpgradeCodes,
                 /* AccountId: */ account.AccountId,
                 /* SeasonId: */ CurrentSeason,
-                /* SeasonNormal: */ new SeasonShortSummary(0, 0, 0, 0),
-                /* SeasonCasual: */ new SeasonShortSummary(0, 0, 0, 0))
+                /* SeasonNormal: */ new SeasonShortSummary(account.PlayerSeasonStatsRanked.Wins + account.PlayerSeasonStatsMidWars.Wins, account.PlayerSeasonStatsRanked.Losses + account.PlayerSeasonStatsMidWars.Losses, 0, 0),
+                /* SeasonCasual: */ new SeasonShortSummary(account.PlayerSeasonStatsRankedCasual.Wins, account.PlayerSeasonStatsRankedCasual.Losses, 0, 0))
             )
+            .AsNoTracking()
             .FirstOrDefaultAsync();
 
         if (data is null)
@@ -57,7 +113,6 @@ public class ShowSimpleStatsHandler : IClientRequesterHandler
             return new NotFoundResult();
         }
 
-        // TODO: include awards once we have stats.
         ShowSimpleStatsResponse showSimpleStatsResponse = new(
             nickname,
             data.TotalLevel,
@@ -65,14 +120,14 @@ public class ShowSimpleStatsHandler : IClientRequesterHandler
             data.NumberOfHeroesOwned,
             data.UnlockedUpgradeCodes.Count(upgrade => upgrade.StartsWith("aa.")),
             data.TotalMatchesPlayed,
-            data.NumberOfMVPAwards,
+            data.CombinedPlayerAwardSummary.MVP,
             data.SelectedUpgradeCodes,
             data.AccountId,
             data.SeasonId,
             data.SeasonNormal,
             data.SeasonCasual,
-            new(),
-            new()
+            data.CombinedPlayerAwardSummary.Top4Names,
+            data.CombinedPlayerAwardSummary.Top4Nums
         );
         return new OkObjectResult(PHP.Serialize(showSimpleStatsResponse));
     }
